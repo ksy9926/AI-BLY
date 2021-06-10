b1c65b79f70768a206b4bf7f97b63a7773df243f
hyunguclear <jhg3522@kookmin.ac.kr>
2021-06-11 01:14:47 +0900

    Feat : landing page

--- client/src/styles/TutorialPageStyle.js
+++ client/src/styles/TutorialPageStyle.js
@@ -1,9 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
-
 import firstpageBackground from "firstpage.png";
+
 const useStyles = makeStyles((theme) => ({
   mobilecontainer: {
-    height: "100vh",
+    height: "101vh",
     display: "grid",
     gridTemplateRows: "repeat(8, 1fr)",
     // gridTemplateColumns: "1fr 2fr 1fr",
@@ -21,51 +21,81 @@ const useStyles = makeStyles((theme) => ({
     left: "0",
   },
   mobileGrid: {
-    gridRow: "2/span 1",
+    gridRow: "3/span 1",
   },
   mobileImageBox: {
     textAlign: "center",
     margin: "20px",
   },
   mobileSubTitle: {
-    color: "var(--color-gray)",
+    color: "#ffffffc4",
     textAlign: "center",
     margin: "10px",
   },
   mobileDescription1: {
-    gridRow: "3/span 1",
-    textAlign: "center",
+    gridRow : "4/span 1",
+    textAlign: "left",
   },
   mobileDescriptionText: {
-    fontSize: "20px",
+    fontSize: "17px",
     textAlign: "center",
     lineHeight: "35px",
     color: "white",
-    textShadow: "white 0px 0 10px",
+    textShadow: "white 0px 0px 2px;",
     
     fontWeight: "1000",
   },
   mobileDescription2: {
-    gridRow: "4/span 2",
+    gridRow : "5/span 2",
     textAlign: "center",
     padding: "20px",
   },
 
   mobileDescriptionSubText: {
-    fontSize: "20px",
-    textAlign: "center",
-    lineHeight: "35px",
-    color: "white",
-    textShadow: "white 0px 0 10px",
+    fontSize: "17px",
+    textAlign: "left",
+    lineHeight: "30px",
+    color: "#ffffffd9",
+    textShadow : "-3px 3px 4px #000000;",
     fontWeight: "1000",
   },
   mobileButtonGrid: {
     gridRow: "7/span 1",
     textAlign: "center",
   },
+  mobileBtn:{
+    textDecoration: "none",
+    background: "var(--color-main-b)",
+    width: "60vw",
+    height :"12vw",
+    borderRadius: "25px",
+    color: "white",
+    fontSize : "15px",
+    underline: "none",
+
+    "&.MuiButton-root:hover": {
+      background: "#fc8979",
+      color: "#f8b9b0",
+    }
+  },
+  mobileBtn2:{
+    textDecoration: "none",
+    background: "white",
+    width: "60vw",
+    height :"12vw",
+    borderRadius: "25px",
+    color: "var(--color-main-b)",
+    fontSize : "15px",
+    underline: "none",
+
+    "&.MuiButton-root:hover": {
+      background: "#fc8979",
+      color: "#f8b9b0",
+    }
+  },
 
   mobileLinkBox: {
-    margin: "15px",
+    margin: "20px",
     fontSize: "15px",
     textAlign: "center",
   },
