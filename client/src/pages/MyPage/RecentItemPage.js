9b4a1682c3b1f7958d73e1405c4f10a9f9b1eb7c
onshore <onshorecoding9@gmail.com>
2021-06-11 21:53:47 +0900

    fix: button url

--- client/src/pages/MyPage/RecentItemPage.js
+++ client/src/pages/MyPage/RecentItemPage.js
@@ -47,7 +47,7 @@ export default function RecentItemPage() {
               <NoItemTemplate
                 title="최근 본 상품이 없어요!"
                 subtitle="클릭하시면 비슷한 상품을 모아서 볼 수 있어요."
-                buttontext="추천상품 보러가기"
+                buttontext="해외직구 상품 보러가기"
                 buttonUrl="/main"
               />
             </Box>
